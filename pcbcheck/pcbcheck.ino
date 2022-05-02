66dbe84b2d9fa8aa97e7f9d1e11c52069b1a2b9b
Christopher Xu <cyx3@illinois.edu>
2022-05-01 22:56:16 -0500

    cleaned more, still 0 and 1s

--- pcbcheck/pcbcheck.ino
+++ pcbcheck/pcbcheck.ino
@@ -204,181 +204,81 @@ uint8_t sendBuffer[4]= {0x0F, 0x00, 0x04, 0x00};
 
 
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-
-  SPI.begin();
-  digitalWrite(PMUXA2, LOW); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, HIGH);
-
-
-//  SPI.transfer(0x 0F 00 04 07); //writes to register 0x0F to disable CRC checking
-  SPI.transfer(writeReg | 0x0F);
-  SPI.transfer(0x00);
-  SPI.transfer(0x04);
-  SPI.transfer(0x07); //CRC checking just this once
-
-
-  
-
-  digitalWrite(PMUXA2, LOW); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, LOW);
-//
-//
-//  digitalWrite(PMUXA2, LOW); 
-//  digitalWrite(PMUXA1, LOW); 
-//  digitalWrite(PMUXA0, HIGH);
-    // set to continuous conversion: 
-//  SPI.transfer(writeReg | 0x00);
-//  SPI.transfer(0x00);
-//  SPI.transfer(0x20); //set bit 5 to 1
-//  SPI.transfer(0x00);
-//  digitalWrite(PMUXA2, LOW); 
-//  digitalWrite(PMUXA1, LOW); 
-//  digitalWrite(PMUXA0, LOW);
-
-  initTMAG5170_forEval();
-
-
-
-//  SPI.transfer(0x0F000407); //writes to register 0x0F to disable CRC checking
-
-
-
-// 1110 0000 0000 0000 0000 0000 1000 1011
-
-// 1110 0000 0000 0000 0000 0000 1000 1010
-
-
-// 1110 0000 0000 0000 0000 0000 1000 1011
-
-// 1110 0000 0000 0000 0100 0000 1000 0011
-
-// 110 0000 0111 1101 1000 0011 1000 0110
-//          0111 1101 1000 0011
-// 110 0000 0111 1101 1000 0011 1000 0110
-
-//reg 0x00
-// 0110 0000 0100 0000 0000 1000 1000 0011
-
-//reg 0x07
-// 0100 0000 0110 0111 0011 0010 0000 1001
-//           0110 0111 0011 0010
-
-//reg 0x06
-// 0100 0000 0111 1101 1000 0011 0000 0000
-//           0111 1101 1000 0011
-
-//reg 0x0F
-// 0100 0000 0000 0000 0101 0100 0000 1001
-
-
-
+  initTMAG5170();
 }
 
 void loop() {
 
   Serial.println("");
-  Serial.println("------------------- F E D C B A 9 8 7 6 5 4 3 2 1 0 ---------------");
-  for(int i=0; i<=0x14; i++){
-      printReg(i);
-  }
+  printReg(ANGLE_RESULT);
+  printReg(MAGNITUDE_RESULT);
 
   delay(50);
 }
 
-void print32(unsigned long val){
-  for(int i=31; i>=0; i--){
-    Serial.print(bitRead(val, i));
-    Serial.print(" ");
-  }
-  Serial.println("");
-}
 
 void printReg(unsigned char address){
   if(address < 0x10){Serial.print(" ");}
   Serial.print(address, HEX);
   Serial.print(": ");
-  digitalWrite(PMUXA2, LOW); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, HIGH);
-  regConfig(readReg, address, 0x0);
-  digitalWrite(PMUXA2, LOW); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, LOW);
+  regSend(readReg, address, 0x0);
   unsigned long ret = (unsigned int)rcvBuffer[0] << 24 | (unsigned int)rcvBuffer[1] << 16 | (unsigned int)rcvBuffer[2] << 8 | (unsigned int)rcvBuffer[3];
-  print32(ret);
-}
-
-
-void sndSPI(){
-  digitalWrite(PMUXA2, LOW); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, HIGH);
-  for (int i = 0; i < 4; i++)
-  {
-    rcvBuffer[i] = SPI.transfer(sendBuffer[i]);
+  for(int i=31-8; i>=8; i--){ //print only the middle 16 bits
+    Serial.print(bitRead(ret, i));
+    Serial.print(" ");
   }
-  digitalWrite(PMUXA2, HIGH); 
-  digitalWrite(PMUXA1, LOW); 
-  digitalWrite(PMUXA0, LOW);
+  Serial.println("");
 }
 
-void initTMAG5170_forEval()
-{
+void initTMAG5170(){
+
+  pinMode(PMUXA2, OUTPUT);
+  pinMode(PMUXA1, OUTPUT);
+  pinMode(PMUXA0, OUTPUT);
+
+  SPI.begin();
+
+  regSend(writeReg, TEST_CONFIG, CRC_DIS_CRCdisabled);
+
   unsigned int data;
   for (byte address = 0x0; address < 0x15; address++){
     switch (address){
     case DEVICE_CONFIG:
       data = CONV_AVG_16x | MAG_TEMPCO_0pd | OPERATING_MODE_ConfigurationMode | T_CH_EN_TempChannelEnabled | T_RATE_sameAsOtherSensors | T_HLT_EN_tempLimitCheckOff | TEMP_COMP_EN_TempCompensationDisenabled;
-      sendBuffer[0] = writeReg | address;
-      sendBuffer[1] = (byte)(data >> 8);
-      sendBuffer[2] = (byte)(data & 0x00ff);
-      sndSPI();
       break;
     case SENSOR_CONFIG:
       data = ANGLE_EN_Y_Z | SLEEPTIME_1ms | MAG_CH_EN_XYZYXenaled | Z_RANGE_25mT | Y_RANGE_25mT | X_RANGE_25mT;
-      sendBuffer[0] = writeReg | address;
-      sendBuffer[1] = (byte)(data >> 8);
-      sendBuffer[2] = (byte)(data & 0x00ff);
-      sndSPI();
       break;
     case SYSTEM_CONFIG:
       data = DIAG_SEL_AllDataPath | TRIGGER_MODE_SPI | DATA_TYPE_32bit | DIAG_EN_AFEdiagnosticsDisabled | Z_HLT_EN_ZaxisLimitCheckoff | Y_HLT_EN_YaxisLimitCheckoff | X_HLT_EN_XaxisLimitCheckoff;
-      sendBuffer[0] = writeReg | address;
-      sendBuffer[1] = (byte)(data >> 8);
-      sendBuffer[2] = (byte)(data & 0x00ff);
-      sndSPI();
       break;
     case TEST_CONFIG:
       data = CRC_DIS_CRCdisabled;
-      sendBuffer[0] = writeReg | address;
-      sendBuffer[1] = (byte)(data >> 8);
-      sendBuffer[2] = (byte)(data & 0x00ff);
-      sndSPI();
       break;
     case MAG_GAIN_CONFIG:
       data = GAIN_SELECTION_noAxisSelected;
-      sendBuffer[0] = writeReg | address;
-      sendBuffer[1] = (byte)(data >> 8);
-      sendBuffer[2] = (byte)(data & 0x00ff);
-      sndSPI();
       break;
     default:
       break;
     }
+    regSend(writeReg, address, data);
   }
-  regConfig(writeReg, DEVICE_CONFIG, DeviceStart);
+  regSend(writeReg, DEVICE_CONFIG, DeviceStart);
 }
 
-void regConfig(unsigned char RW, unsigned char address, unsigned int data){
+void regSend(unsigned char RW, unsigned char address, unsigned int data){
   sendBuffer[0] = RW | address;
   sendBuffer[1] = (unsigned char)(data >> 8);
   sendBuffer[2] = (unsigned char)(data & 0x00ff);
-  sendBuffer[3] = 0x0F;
+  sendBuffer[3] = 0x07; //CRC specifically for disabling CRC
 
-  sndSPI();
+  digitalWrite(PMUXA2, LOW); 
+  digitalWrite(PMUXA1, LOW); 
+  digitalWrite(PMUXA0, HIGH);
+  for (int i = 0; i < 4; i++){
+    rcvBuffer[i] = SPI.transfer(sendBuffer[i]);
+  }
+  digitalWrite(PMUXA2, HIGH); //selects mux4 which is unused
+  digitalWrite(PMUXA1, LOW); 
+  digitalWrite(PMUXA0, LOW);
 }
