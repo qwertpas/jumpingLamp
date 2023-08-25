f257b988347510b76974f21feb320e41867142b4
Christopher Xu <christopher.y.x@gmail.com>
2023-08-25 16:11:08 -0500

    generate test commands for 2 addresses

--- v3_sp23/teensy4/thumbstick/thumbstick.ino
+++ v3_sp23/teensy4/thumbstick/thumbstick.ino
@@ -3,103 +3,107 @@
 // RS485_A of Ø32 goes to TX of Serial2 on Teensy
 
 
+#include <Metro.h> // periodic function execution
+#include "comdef.h" // periodic function execution
+
+
 #define STICK_X A0
 #define STICK_Y A1
 
-int led = LED_BUILTIN;
 
+Metro metro_500Hz = Metro(1);  // 2ms, 500Hz
 int16_t stick_x_0 = 0;
 int16_t stick_y_0 = 0;
+uint8_t uart2_TX[7] = {0}; //command RS485 to Ø32
 
-void setup()
-{
 
-  pinMode(led, OUTPUT);
+char uart0_RX[2048] = {0}; //USB serial input from laptop
+char uart1_RX[2048] = {0}; //debug UART from Ø32
+
+
+
+void setup() {
+
   Serial.begin(115200);       // start serial for serial monitor output (println)
   Serial1.begin(115200);      // start serial for input from the STM32
-  Serial2.begin(115200);      // start serial for RS485 output 
+  
+  Serial2.begin(115200);      // start serial for RS485 output
+//  Serial2.begin(230400);      // start serial for RS485 output (not captured by scopy)
 
   stick_x_0 = analogRead(STICK_X);
   stick_y_0 = -analogRead(STICK_Y);
 
   delay(500);
-  digitalWrite(led, HIGH);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
+uint8_t count = 0;
 
-uint8_t i2c_TX[2] = {1,1}; //out to stm32
+void loop() {
 
-int count = 0;
-int transmit_i2c = 0;
+  if (metro_500Hz.check()) {
 
-void loop()
-{
-  char uart0_RX[2048] = {0}; //in from usb
-  char uart1_RX[2048] = {0}; //in from stm32
+    //Joystick input
+    int16_t stick_x = analogRead(STICK_X) - stick_x_0;
+    int16_t stick_y = -analogRead(STICK_Y) - stick_y_0; //between -550 and 550
+
+    if(count % 2 == 0){
+      uart2_TX[0] = CMD_SET_VOLTAGE | 3;
+      uart2_TX[1] = count % 0b01111111;
+      Serial2.write(uart2_TX, 2);
+    }else{
+      uart2_TX[0] = CMD_SET_VOLTAGE | 4;
+      uart2_TX[1] = count % 0b01111111;
+      Serial2.write(uart2_TX, 2);
+    }
+    
+   
+    count += 1;
 
 
-  //Joystick input
-  int16_t stick_x = analogRead(STICK_X) - stick_x_0;
-  int16_t stick_y = -analogRead(STICK_Y) - stick_y_0; //between -550 and 550
+    
+//    uart2_TX[1] = abs(stick_x) >> 2;
+//    uart2_TX[2] = 0xAA;
 
-  //USB serial input
-  if(Serial.available()) {
-    for(int i=0; i < sizeof(uart0_RX); i++){
-      if(Serial.available()){
+//    uart2_TX[0] = count;
+//    uart2_TX[1] = count+1;
+//    uart2_TX[2] = count+2;
+//    uart2_TX[3] = count+3;
+//    uart2_TX[4] = count+4;
+//    uart2_TX[5] = count+5;
+//    uart2_TX[6] = count+6;
+//    Serial2.write(uart2_TX, 7);
+
+  }
+
+
+
+  //USB serial input goes into uart0_RX
+  if (Serial.available()) {
+    for (uint8_t i = 0; i < sizeof(uart0_RX); i++) {
+      if (Serial.available()) {
         uart0_RX[i] = Serial.read();
       }
-      if(uart0_RX[i] == '\n') break;
+      if (uart0_RX[i] == '\n') break;
     }
     Serial.println(uart0_RX);
-
-    int num = (int)(uart0_RX[0] - '0');
-    if(num >= 0 && num <= 9){
-      i2c_TX[0] = num;
-      transmit_i2c = 1;
-    }
   }
-    
-    
-  //UART1 receive all or until buffer filled
-  int printall = 1;
-  if(Serial1.available()) {
-    for(int i=0; i < sizeof(uart1_RX); i++){
-      if(Serial1.available()){
+
+
+
+  //UART1 passthrough for debugging
+  if (Serial1.available()) {
+    for (uint8_t i = 0; i < sizeof(uart1_RX); i++) {
+      if (Serial1.available()) {
         uart1_RX[i] = Serial1.read();
-      }else{
+      } else {
         break;
       }
     }
-    
-    //print to usb
-    if(printall){
-      Serial.print(uart1_RX);
-    }else{
-      int start_i = -1;
-      for(int i=0; i < sizeof(uart1_RX); i++){
-        if(uart1_RX[i] == '\n'){
-          if(start_i == -1){
-            start_i = i; //found the first newline
-          }else{
-            Serial.print('\n'); //found the second newline
-            break;
-          }
-        }else if(start_i != -1){ //first newline has been found
-          Serial.print(uart1_RX[i]);
-        }
-      }
-    }
+    Serial.print(uart1_RX);
   }
 
-  if(count % 10 == 0){
-    
-      Serial2.write(abs(stick_y) >> 2);
-//      Serial2.write(0xBB);
 
-      Serial.println(stick_y);
-    
-  }
-  
-    delay(1);
-    count++;
 }
